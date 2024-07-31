48b40fe80a65ed565aa75aca5c496074bee5a906
DominiEduardo <domini.edu@gmail.com>
2024-07-31 19:51:24 -0300

    teste

---
+++ vscode/soma.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 20
+
+soma = num1 + num2
+
+media = soma / 2
+
+print(f"A soma dos números é: {soma}")
+print(f"A média dos números é: {media:.2f}")
+
+resultado_final = soma + media
+print(f"A soma da soma com a média é: {resultado_final:.2f}")
