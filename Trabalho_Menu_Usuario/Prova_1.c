17cd0e8104b328e2a3193141a9b4dc993d79206f
DominiEduardo <domini.edu@gmail.com>
2024-08-28 22:17:44 -0300

    new commit

--- Trabalho_Menu_Usuario/Prova_1.c
+++ Trabalho_Menu_Usuario/Prova_1.c
@@ -1,96 +1,87 @@
 #include <stdio.h>
 
-double soma(double a, double b)
-{
+double soma(double a, double b) {
     return a + b;
 }
 
-double subtracao(double a, double b)
-{
-    return a - b;
-}
-
-double multiplicacao(double a, double b)
-{
-    return a * b;
-}
-
-double divisao(double a, double b)
-{
-    if (b != 0)
-    {
-        return a / b;
+double subtracao(double a, double b) {
+        return a - b;
     }
-    else
-    {
-        printf("Nao eh possivel dividir com valor zero. \n");
-        return 0;
-    }
-}
 
-double potenciacao(double base, double expoente)
-{
-    double resultado = 1;
-    for (int i = 0; i < expoente; i++)
-    {
-        resultado *= base;
+double multiplicacao(double a, double b) {
+        return a * b;
     }
-    return resultado;
-}
 
-double fatorial(int n) {
-    if (n == 0) {
-        return 1; 
-    } else {
+double divisao(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            bandeira = 0;
+            printf("Nao eh possivel dividir com valor zero. \n");
+            return 0;
+            }
+    }
+
+double potenciacao(double base, int expoente) {
         double resultado = 1;
-        for (int i = 1; i <= n; i++) {
-            resultado *= i;
+        for (int i = 0; i < expoente; i++) {
+            resultado *= base;
         }
         return resultado;
     }
+
+double fatorial(int n) {
+        if (n == 0) { return 1;
+
+        } else {
+            double resultado = 1;
+            for (int i = 1; i <= n; i++) {
+                resultado *= i;
+            }
+            return resultado;
+        }
+    }
+
+double raizQuadrada(double a) {
+        double n;
+        double chute = a;
+
+        if (chute == 0) {
+            return (0);
+        } else {
+            //chute = 0.5 * (a+(a/a));
+            for (n = 0; n < 100; n++) {
+                chute = 0.5 * (chute + (a / chute));
+            }
+            return (chute);
+        }
+
+    }
+
+double equacSG(double a, double b, double c) {
+        double delta = b * b - 4 * (a * c);
+
+        if (delta < 0) {
+                printf("Nao e possivel este valor. \n");
+} else if (delta == 0) {
+    double x = -b / (2 * a);
+    printf("Unica solucao. %.5f \n", x);
+    } else {
+    double x1 = (-b + raizQuadrada(delta)) / 2 * a;
+    double x2 = (-b - raizQuadrada(delta)) / 2 * a;
+    printf("Valor de X 1 = %.5f e X 2 = %.5f\n", x1, x2);
+    }
 }
 
-double raizQuadrada(double a)
-{
-    double x = a;
-    double y = (x * 1) / 2;
-    while (y < x)
-    {
-        x = y;
-        y = (x * a / x) / 2;
-    }
-    return x;
-}
+void bandeira = 0;
 
-double equacSG(double a, double b, double c)
-{
-    double delta = b * b - 4 * a * c;
-    if (delta < 0)
-    {
-        printf("Nao e possivel este valor. \n");
-    }
-    else if (delta == 0)
-    {
-        double x = -b / (2 * a);
-        printf("Unica solucao. %.5f \n", x);
-    }
-    else
-    {
-        double x2 = (-b * raizQuadrada(delta)) / (2 * a);
-        double x3 = (-b - raizQuadrada(delta)) / (2 * a);
-        printf("Valor de numero 1 = %.5f e numero 2 = %.5f\n", x2, x3);
-    }
-}
+    int main() {
+        int escolhaUsuario;
+        double num1, num2, num3;
+        int expoente;
 
-int main()
-{
-
-    int escolhaUsuario;
-    double num1, num2;
-
-    while (1)
-    {
-        printf("Menu para o usuario decidir sua operacao a ser calculada: \n");
+        while (1) {
+                printf("\n Menu para o usuario decidir sua operacao a ser calculada: \n");
         printf("1. Sair Do Programa \n");
         printf("2. Somar \n");
         printf("3. Subtrair \n");
@@ -99,57 +90,66 @@ int main()
         printf("6. Potenciacao \n");
         printf("7. Raiz Quadrada \n");
         printf("8. Fatorial \n");
-        printf("9. MDC entre dois numeros \n");
+        printf("9. Equacao do Segundo Grau \n");
+        printf("11. MDC entre dois numeros \n");
         printf("10. MDM entre dois numeros \n");
-        printf("11. Equacao do Segundo Grau \n");
         printf("Escolha a sua opcao: ");
         scanf("%d", &escolhaUsuario);
 
-        switch (escolhaUsuario)
-        {
-        case 1:
-            printf("O usuario saiu do menu/operacao \n");
+        switch (escolhaUsuario) {
+            case 1: printf("O usuario saiu do menu/operacao \n");
             return 0;
             break;
-        case 2:
-            printf("Digite dois numeros para somar: ");
-            scanf("%lf %lf", &num1, &num2);
-            printf("O resultado da soma eh: %.5f \n", soma(num1, num2));
-        case 3:
-            printf("Digite dois numeros para subtrair: ");
-            scanf("%lf %lf", &num1, &num2);
-            printf("O resultado da subtracao eh: %.5f \n", subtracao(num1, num2));
-            break;
-        case 4:
-            printf("Digite dois numeros para multiplicar: ");
-            scanf("%lf %lf", &num1, &num2);
-            printf("O resultado da multiplicacao eh: %.5f \n", multiplicacao(num1, num2));
-            break;
-        case 5:
-            printf("Digite dois numeros para dividir: ");
-            scanf("%lf %lf", &num1, &num2);
-            printf("O resultado da divisao eh: %.5f \n", divisao(num1, num2));
-            break;
-        case 6:
-            printf("Digite a base da potencia: \n");
-            scanf("%lf", &num1);
-            printf("Digite o expoente da potencia: ");
-            scanf("%lf", &num2);
-            printf("O resultado da potenciacao eh: %.5f \n", potenciacao(num1, num2));
-            break;
-        case 7:
-            printf("Digite um numero inteiro para calcular a raiz quadrada: ");
-            scanf("%lf", &num1);
-            printf("O resultado da raiz quadrada escolhida eh: %.5f \n", raizQuadrada(num1));
-            break;
-        case 8:
-            printf("Digite um numero inteiro para calcular a fatoriacao: ");
-            scanf("%lf", &num1);
-            printf("O resultado da raiz quadrada escolhida eh: %.5f \n", fatorial(num1));
-            break;
-        default:
-            printf("opcao inexistente.");
-        }
+
+    case 2: printf("Digite dois numeros para somar: ");
+    scanf("%lf %lf", &num1, &num2);
+    printf("O resultado da soma eh: %.5f \n", soma(num1, num2));
+    break;
+
+    case 3: printf("Digite dois numeros para subtrair: ");
+    scanf("%lf %lf", &num1, &num2); printf("O resultado da subtracao eh: %.5f \n",
+    subtracao(num1, num2));
+    break;
+
+    case 4: printf("Digite dois numeros para multiplicar: ");
+    scanf("%lf %lf", &num1, &num2);
+    printf("O resultado da multiplicacao eh: %.5f \n", multiplicacao(num1, num2));
+    break;
+
+    case 5: printf("Digite dois numeros para dividir: ");
+    scanf("%lf %lf", &num1, &num2);
+    if (bandeira == 1) {
+    printf("O resultado da divisao eh: %.5f \n", divisao(num1, num2));
+    }
+    else {
+        bandeira = 0;
+        printf ("Nao eh possivel a divisao com o valor 0.");
+    }
+    break;
+
+    case 6: printf("Digite a base da potencia: \n");
+    scanf("%lf", &num1);
+    printf("Digite o expoente da potencia: ");
+    scanf("%d", &expoente);
+    printf("O resultado da potenciacao eh: %.5lf \n", potenciacao(num1, expoente));
+    break;
+
+    case 7:
+    printf("Digite um numero inteiro para calcular a raiz quadrada: ");
+    scanf("%lf", &num1); printf("O resultado da raiz quadrada escolhida eh: %.5f \n", raizQuadrada(num1));
+    break;
+
+    case 8: printf("Digite um numero inteiro para calcular a fatoriacao: "); scanf("%lf", &num1);
+    printf("O resultado da fatoracao eh: %.5f \n", fatorial(num1));
+    break;
+
+    case 9:
+    printf("Digite tres numeros para calcular a equacao de segundo grau: ");
+    scanf("%lf %lf %lf", &num1, &num2, &num3);
+    printf("O resultado da equacao do 2° grau eh: %.5f \n", equacSG(num1, num2, num3));
+    break;
+    default: printf("opcao inexistente. \n");
+}
     }
     return 0;
-}
+        }
